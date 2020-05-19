a5249fd464da670850836ddf2df246cccc51b35a
Anirban Gorain <gorainanirban@gmail.com>
2020-05-19 14:20:14 +0530

    _add_Sign_Up_Function_And_Take_User_Name_Feature

--- _source_Code.c
+++ _source_Code.c
@@ -3,6 +3,7 @@
 #include<stdio.h>
 #include<conio.h>
 #include<windows.h>
+#include<string.h>
 
 // Function prototype.
 void gotoxy(int ,int );
@@ -11,15 +12,16 @@ void _border(int, int);
 void _red(void);
 void _green(void);
 void _reset(void);
+int _sign_Up(void);
 
 int main (void){
     // Border
-    _border(28, 121);
+    // _border(28, 121);
 
     // Password
-    int _return_Value;
-    _return_Value = _registration_Menu();
-
+    // int _return_Value;
+    // _return_Value = _registration_Menu();
+    _sign_Up();
     // printf("DONE");
     // getch();
     return 0;
@@ -71,15 +73,22 @@ void _border(int _height, int _width){
 }
 // Color functions
 void _red(void){
+
     printf("\033[0;31m");
+
 }
 void _green(void){
+
     printf("[0;32m");
+
 }
 void reset(void){
+
     printf("[0m");
+
 }
 // ------------------------------------------ REGISTRATION SECTION ---------------------------------------------
+// Registration menu.
 char _registration_Menu(void){
 
     // This variable value will be return from here.
@@ -129,6 +138,7 @@ char _registration_Menu(void){
         // When user will exceed maximum number of chance program will be close.
 
         if(_maximum_Chance > 4){
+
             _red();
             Beep(800, 1000);
             gotoxy(30, 15);
@@ -140,6 +150,74 @@ char _registration_Menu(void){
 
         _maximum_Chance++;
         _registration_Menu();
+
+    }
+
+}
+// Sign-up time.
+int _sign_Up(void){
+
+    system("cls");
+    _border(28, 121);
+    gotoxy(45, 2);
+    printf("--WELCOME TO THE SIGN-UP--");
+    gotoxy(32, 3);
+    printf("-----------------------------------------------------------");
+    // First check may stored any previous Signed-up data and this section of code for this
+
+    // ------------- --------  ----------- ---------------- -------------
+
+
+    // If don't having any Signed-up data the this section of code for this.
+
+    // Taking the user name.
+
+    char _user_Name[20];
+    int _user_Name_Lenght;
+    int _exceed_Time = 0;
+    int _update_Y_Axis = 8;
+
+    while(4){
+
+        _exceed_Time++;
+        _update_Y_Axis++;
+
+        // If the maximum number of attempt are exceed the program will be close.
+
+        if (_exceed_Time == 4 ){
+
+            _red();
+            gotoxy(18, 26);
+            printf("YOU EXCEED THE MAXIMUM NUMBER OF TIME ATTEMPT, TRY AGAIN LATER :)");
+            getch();
+            exit(1);
+
+        }
+
+        gotoxy(18, _update_Y_Axis);
+        printf("ENTER YOUR USER-NAME, USER-NAME LENGHT MUST BE GRATER 5 CHRACTER AND LESS 20 CHRACTER: ");
+
+        _update_Y_Axis++;
+        gotoxy(18, _update_Y_Axis);
+        fflush(stdin);
+        gets(_user_Name);
+
+        _user_Name_Lenght = strlen(_user_Name);
+
+        // If the user name satisfied all the criteria then  this loop will be break.
+
+        if( _user_Name_Lenght > 5 && _user_Name_Lenght < 20 ){
+
+            break;
+
+        }
+
+        if (_update_Y_Axis == 10)
+        {
+
+            _update_Y_Axis--;
+
+        }
 
     }
 
